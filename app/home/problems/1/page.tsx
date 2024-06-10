17f7f107604638d1e2891fddf4075c52bdedb87e
chicken-04 <henryctran2007@gmail.com>
2024-06-10 13:04:26 -0700

    test commit

--- app/home/problems/1/page.tsx
+++ app/home/problems/1/page.tsx
@@ -1,16 +1,22 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
-    const router = useRouter();
-    return (
-        <main className="w-screen min-h-screen flex-col flex bg-orange-300">
-            <div className="bg-white text-3xl p-4 font-bold text-orange-300 w-full flex flex-row justify-center">
-                <button onClick={() => router.push('/home')} className="absolute left-5 text-white hover:bg-orange-500 hover:text-gray-300 text-4xl px-3 rounded-2xl pb-1 bg-orange-300">{'⌂'}</button>
-                <p>Project Sense:</p>
-                <p className='absolute right-5'>username</p>
-            </div>
-        </main>
-    )
+  const router = useRouter();
+  const testcommit = 1;
+  return (
+    <main className="w-screen min-h-screen flex-col flex bg-orange-300">
+      <div className="bg-white text-3xl p-4 font-bold text-orange-300 w-full flex flex-row justify-center">
+        <button
+          onClick={() => router.push("/home")}
+          className="absolute left-5 text-white hover:bg-orange-500 hover:text-gray-300 text-4xl px-3 rounded-2xl pb-1 bg-orange-300"
+        >
+          {"⌂"}
+        </button>
+        <p>Project Sense:</p>
+        <p className="absolute right-5">username</p>
+      </div>
+    </main>
+  );
 };
 export default Home;
