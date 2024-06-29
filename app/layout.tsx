719691d8f206f835a96f9f9d4f4d211e82f06550
Henry Tran <henryctran2007@gmail.com>
2024-06-29 17:36:25 -0500

    head movement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Project Sense</title>
+        <meta name="description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
+        <meta property="og:title" content="Project Sense" />
+        <meta property="og:description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
+        <meta property="og:image" content="https://project-sense.vercel.app/opengraph-image.png" />
+        <meta property="og:url" content="https://project-sense.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Project Sense" />
+        <meta name="twitter:description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
+        <meta name="twitter:image" content="https://project-sense.vercel.app/twitter-image.png" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
