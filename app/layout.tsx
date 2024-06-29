5601515161998d397300d6db7a47c448b96a6ee0
Henry Tran <henryctran2007@gmail.com>
2024-06-29 02:44:09 -0500

    twitter testing agian

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,20 +7,9 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-   title: "Project Sense",
+  title: "Project Sense",
   description: "Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!",
   manifest: "/manifest.json",
-  icons: {
-    apple: "/icon-512x512.png"
-  },
-  themeColor: "#fdba74",
-  twitter: {
-    card: "summary_large_image",
-    site: "https://project-sense.vercel.app/", // Replace with your Twitter handle
-    title: "Project Sense",
-    description: "Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!",
-    images: "/projectSenseLogo-1200.png", // URL to your Twitter card image
-  },
 };
 
 export default function RootLayout({
@@ -35,15 +24,15 @@ export default function RootLayout({
         <meta name="title" content="Project Sense" />
         <meta name="description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://project-sense.vercel.app/" />
+        <meta property="twitter:url" content="https://project-sense.vercel.app/" />
         <meta property="og:title" content="Project Sense" />
         <meta property="og:description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
-        <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+        <meta property="og:image" content="/projectSenseLogo-1200.png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
         <meta property="twitter:title" content="Project Sense" />
         <meta property="twitter:description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
-        <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+        <meta property="twitter:image" content="/projectSenseLogo-1200.png" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
