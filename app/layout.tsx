2c0f81641d6ffed1b4d7df9b923190c8f56453a3
Henry Tran <henryctran2007@gmail.com>
2024-06-29 02:22:56 -0500

    layout clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,6 @@ export const metadata: Metadata = {
   title: "Project Sense",
   description: "Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!",
   manifest: "/manifest.json",
-  icons: {
-    apple: "/icon-512x512.png"
-    
-  },
-  themeColor: "#fdba74"
 };
 
 export default function RootLayout({
@@ -24,13 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Project Sense</title>
-        <meta name="description" content="Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!" />
-        <meta property="og:image" content="/icon-512x512.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:image" content="https://www.apple.com/v/iphone/home/t/images/home/og.png?201610171354" />
-      </Head>
+      {/* <Head>
+        
+      </Head> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
