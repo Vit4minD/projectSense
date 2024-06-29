cdc06f506991b4b9f68f34f1ae619bec8b5b495c
Henry Tran <henryctran2007@gmail.com>
2024-06-29 18:14:26 -0500

    next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,24 @@
+// next-seo.config.js
+
+export default {
+    title: 'Project Sense',
+    description: 'Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!',
+    openGraph: {
+      url: 'https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/project-sense.vercel.app/Project%20Sense/https%3A%2F%2Fproject-sense-6ysr7llve-henry-trans-projects.vercel.app%2Fopengraph-image.png%3F83e7d7e34c916d3f/og.png',
+      type: 'website',
+      title: 'Project Sense',
+      description: 'Practice TMSCA/UIL Number Sense Questions using Project Sense and compete against others through a variety of different tricks (including tutorials)!',
+      images: [
+        {
+          url: 'https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/project-sense.vercel.app/Project%20Sense/https%3A%2F%2Fproject-sense-6ysr7llve-henry-trans-projects.vercel.app%2Fopengraph-image.png%3F83e7d7e34c916d3f/og.png',
+          alt: 'Project Sense OpenGraph Image',
+        },
+      ],
+    },
+    twitter: {
+      cardType: 'summary_large_image',
+      handle: '@yourtwitterhandle', // Replace with your Twitter handle
+      site: '@yoursite', // Replace with your Twitter site
+    },
+  };
+  
