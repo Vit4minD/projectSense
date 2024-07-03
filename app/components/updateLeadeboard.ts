19a061939a4a8b80e65911d42dbdeb6bbc2bc450
Henry Tran <henryctran2007@gmail.com>
2024-07-02 20:41:20 -0500

    fixed sort method

--- app/components/updateLeadeboard.ts
+++ app/components/updateLeadeboard.ts
@@ -7,10 +7,6 @@ import {
   writeBatch,
 } from "firebase/firestore";
 
-interface User {
-  [key: string]: any;
-}
-
 interface Leaderboard {
   scores: { [key: string]: string };
 }
@@ -27,56 +23,49 @@ export default async function updateLeaderboard(
     const userRef = doc(colRef, email);
     const scoresRef = doc(boardRef, String(trick));
 
-    const batch = writeBatch(db);
-
-    const userSnap: DocumentSnapshot<User> = await getDoc(userRef);
+    const userSnap = await getDoc(userRef);
     if (!userSnap.exists()) {
       throw new Error("User document not found.");
     }
 
+    const batch = writeBatch(db);
     const userData = userSnap.data();
 
-    if (userData && userData[trick]) {
-      if (compareTimes(userData[trick], time) === 1) {
-        batch.update(userRef, { [trick]: time });
-      }
-    } else {
+    const oldTime = userData[trick];
+    const newTime = time;
+    const update = isFasterTime(oldTime, newTime)
+
+    if (update) {
       batch.update(userRef, { [trick]: time });
+      const scoresSnap = (await getDoc(
+        scoresRef
+      )) as DocumentSnapshot<Leaderboard>;
+      if (scoresSnap.exists()) {
+        const scoresData = scoresSnap.data();
+        const scoreList = { ...scoresData?.scores, [email]: time };
+        batch.update(scoresRef, { scores: scoreList });
+      } else {
+        const newScores = { scores: { [email]: time } };
+        batch.set(scoresRef, newScores);
+      }
+      await batch.commit();
     }
-
-    const scoresSnap = (await getDoc(
-      scoresRef
-    )) as DocumentSnapshot<Leaderboard>;
-    if (scoresSnap.exists()) {
-      const scoresData = scoresSnap.data();
-      const scoreList = { ...scoresData?.scores, [email]: time };
-      batch.update(scoresRef, { scores: scoreList });
-    } else {
-      const newScores = { scores: { [email]: time } };
-      batch.set(scoresRef, newScores);
-    }
-
-    await batch.commit();
-
-    console.log("Document successfully updated!");
   } catch (error) {
     console.error("Error updating leaderboard:", error);
-    throw error; 
+    throw error;
   }
 }
 
-function compareTimes(time1: string, time2: string): number {
-  const [min1, sec1, ms1] = time1.split(":").map((x) => parseFloat(x));
-  const [min2, sec2, ms2] = time2.split(":").map((x) => parseFloat(x));
+function isFasterTime(oldTime: string, newTime: string): boolean {
+  if (!oldTime) return true;
+  const parseTime = (time: string) => {
+    const [min, sec] = time.split(":");
+    const [seconds, milliseconds] = sec.split(".");
+    return parseFloat(min) * 60 * 1000 + parseFloat(seconds) * 1000 + parseFloat(milliseconds);
+  };
 
-  const totalMs1 = min1 * 60000 + sec1 * 1000 + ms1;
-  const totalMs2 = min2 * 60000 + sec2 * 1000 + ms2;
+  const oldTimeMs = parseTime(oldTime);
+  const newTimeMs = parseTime(newTime);
 
-  if (totalMs1 < totalMs2) {
-    return -1;
-  } else if (totalMs1 > totalMs2) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return newTimeMs < oldTimeMs;
 }
